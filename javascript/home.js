2e9e29dcee83ea5e2330f983b8b680c712e89c9d
eganaww <geminggg81@gmail.com>
2024-05-30 09:05:14 +0700

    add sticky nav

--- javascript/home.js
+++ javascript/home.js
@@ -4,3 +4,10 @@ const navMenu = document.querySelector('.nav-menu');
 navIcon.addEventListener('click', () => {
     navMenu.classList.toggle('active');
 });
+
+
+window.addEventListener("scroll", () => {
+    let nav = document.querySelector(".header");
+    nav.classList.toggle("sticky", scrollY > 0);
+  });
+  
